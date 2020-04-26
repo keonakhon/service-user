d544535aac6e5d66df04fb0792670c5de199123d
Keonakhon <keonakhon.kawan@outlook.com>
2020-04-27 02:13:55 +0700

    Add initial graphql code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
-import express from 'express';
+import express from "express";
+import graphqlHTTP from "express-graphql";
+import { buildSchema } from "graphql";
+
 const app = express();
 const port = 8080; // default port to listen
 
-// define a route handler for the default home page
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  hello: () => "Hello world!"
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    graphiql: true
+  })
+);
 
 // start the Express server
 app.listen(port, () => {
