5f11f24e0db402b1b4c34f48b1bd66ab970e41cd
Keonakhon <keonakhon.kawan@outlook.com>
2020-07-27 20:30:02 +0700

    Add other query

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,13 @@
 // Provide resolver functions for your schema fields
+interface FacebookLogin {
+  fb_token: { fb_token: string };
+}
+
 const resolvers = {
-  hello: () => "Hello world!"
+  hello: () => "Hello world!",
+  fbLogin: ({ fb_token }: FacebookLogin): object => {
+    return { refresh_token: fb_token };
+  }
 };
 
 export default resolvers;
--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -2,9 +2,14 @@ import { buildSchema } from "graphql";
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
+  type LoginWithFB {
+    refresh_token: String
+  }
+
   type Query {
     hello: String
-  }
+    fbLogin(fb_token: String!): LoginWithFB
+  },
 `);
 
 export default schema;
