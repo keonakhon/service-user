d133626249c7a363792d9463ab5b1246d4acd927
Keonakhon <keonakhon.kawan@outlook.com>
2020-08-11 18:09:42 +0700

    Add checking if status is connected

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -17,21 +17,22 @@ const resolvers = {
   // Facebook Login
   fbLogin: async ({ status, authResponse }: FacebookLogin) => {
     try {
-      const userToken =
-        "EAAIqZCPH6Yj4BAKtWUMsbaUrGIR5crYSkGSmy1bvJ2xCxVSM5vNmM7kxKrilLl8FZB5SqYAVzqlj0j16R26jyTbDv2GZCsgmg40fpc3FSdTVZAnMYlnNFZBKMecUiiNG0T3Mvt5z1f9LtoZBJHvr52EUBqdb0HDcf85r85JYFfHsgCy9JMiTyu3ZCUDY0EWZCp9gL585NzmgCAZDZD";
+      if (status === "connected") {
+        const userToken = authResponse.accessToken;
 
-      // Inspect Token from facebook api
-      const indpectorToken = await axios.get(
-        `https://graph.facebook.com/debug_token?input_token=${userToken}&access_token=${process.env.facebook_app_access_token}`
-      );
-      const userID = indpectorToken.data.data.user_id;
+        // Inspect Token from facebook api
+        const indpectorToken = await axios.get(
+          `https://graph.facebook.com/debug_token?input_token=${userToken}&access_token=${process.env.facebook_app_access_token}`
+        );
+        const userID = indpectorToken.data.data.user_id;
 
-      // User Data from facebook api
-      const userData = await axios.get(
-        `https://graph.facebook.com/${userID}?fields=id,name&access_token=${userToken}`
-      );
+        // User Data from facebook api
+        const userData = await axios.get(
+          `https://graph.facebook.com/${userID}?fields=id,name&access_token=${userToken}`
+        );
 
-      return { status };
+        return { status };
+      }
     } catch (err) {
       // console.error(err);
     }
