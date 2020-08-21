7c113c450119a9c7721795daef31695e643dfccd
Keonakhon <keonakhon.kawan@outlook.com>
2020-08-22 01:03:24 +0700

    Add helper class to check user data exists

---
+++ src/helpers/user.ts
@@ -0,0 +1,59 @@
+/* User Helper */
+import axios from "axios";
+
+// Models
+import UserModel from "./../models/user";
+import UserTokenSchema from "./../models/user_token";
+
+class UserHelper {
+  accessToken: string;
+  expiresIn: string;
+  signedRequest: string;
+  userID: string;
+
+  constructor(
+    accessToken: string,
+    expiresIn: string,
+    signedRequest: string,
+    userID: string
+  ) {
+    this.accessToken = accessToken;
+    this.expiresIn = expiresIn;
+    this.signedRequest = signedRequest;
+    this.userID = userID;
+
+    this.CheckUser();
+  }
+
+  /* Check if user already existed */
+  async CheckUser() {
+    try {
+      const fbUserToken = this.accessToken;
+
+      // Inspect Token from facebook api
+      const fbIndpectorToken = await axios.get(
+        `https://graph.facebook.com/debug_token?input_token=${fbUserToken}&access_token=${process.env.facebook_app_access_token}`
+      );
+      const userID = fbIndpectorToken.data.data.user_id;
+
+      // User Data from facebook api
+      const fbUserData = await axios.get(
+        `https://graph.facebook.com/${userID}?fields=id,name&access_token=${fbUserToken}`
+      );
+      const fbUserID = fbUserData.data.id;
+      const fbUserFullname = fbUserData.data.id;
+
+      const userData = await UserModel.findOne({
+        "facebook.facebook_id": fbUserID
+      });
+
+      return userData;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  async login() {}
+}
+
+export default UserHelper;
