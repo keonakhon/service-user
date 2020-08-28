08c2a59fff02aa2ad55dc22e71d855092b9f35f9
Keonakhon <keonakhon.kawan@outlook.com>
2020-08-29 01:18:10 +0700

    Add more mock keys

--- src/configs/keys.ts
+++ src/configs/keys.ts
@@ -1,3 +1,8 @@
 export default {
-  uri: "mongodb://localhost:5546/user"
+  uri: "mongodb://localhost:5546/user",
+  access_token_secret: "423kjifos3rwsfd",
+  refresh_token_secret: "fdsfjwoejri233sfwej",
+  /* Format - Day * Hour * Minute * Second */
+  access_token_life: 50 * 24 * 60 * 60,
+  refresh_token_life: 60 * 24 * 60 * 60
 };
