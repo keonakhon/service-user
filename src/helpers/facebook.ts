0e3074a15889568a30a472ec443527c9ca216ade
Keonakhon <keonakhon.kawan@outlook.com>
2020-09-01 17:23:57 +0700

    Delete check user from constructor

--- src/helpers/facebook.ts
+++ src/helpers/facebook.ts
@@ -24,8 +24,6 @@ class FacebookHelper {
     this.signedRequest = signedRequest;
     this.userID = userID;
     this.ipAddress = ipAddress;
-
-    this.CheckUser();
   }
 
   /* Check if user already existed */
