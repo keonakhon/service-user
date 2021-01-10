8a262c2e34596a2b76174e2d93ddff483c51f056
Keonakhon <keonakhon.kawan@outlook.com>
2021-01-10 23:01:07 +0700

    Add condition to remove bearer & Update test file

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -10,17 +10,24 @@ import UserModel from "../models/user";
 const AuthHelper = (context: any) => {
   try {
     const { token } = context;
-    return jwt.verify(
-      token,
-      keys.access_token_secret,
-      (err: any, decoded: any) => {
-        if (err) return false;
 
-        const userData = UserModel.findById(decoded._id);
+    if (token.startsWith("Bearer ")) {
+      const accessToken = token.substring(7, token.length);
 
-        return userData;
-      }
-    );
+      return jwt.verify(
+        accessToken,
+        keys.access_token_secret,
+        (err: any, decoded: any) => {
+          if (err) return false;
+
+          const userData = UserModel.findById(decoded._id);
+
+          return userData;
+        }
+      );
+    } else {
+      return false;
+    }
   } catch (err) {
     return false;
   }
