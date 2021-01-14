41dcd2d5e27b67ce04ef887bed3bfd8ce987320a
Keonakhon <keonakhon.kawan@outlook.com>
2021-01-14 22:46:54 +0700

    Add token without email and optimize to func

--- __test__/util/token.ts
+++ __test__/util/token.ts
@@ -10,22 +10,34 @@ const fbAccessTokenTestUser = async () => {
     const fbGraphQLURL = "https://graph.facebook.com";
     let appID = process.env.facebook_app_id;
     let appSecret = process.env.facebook_app_secret;
-    const fileName = "__test__/util/static/facebook_token.txt";
+    const fbTokenWithEmailFile =
+      "__test__/util/static/facebook/token_w_email.txt";
+    const fbTokenWithoutEmailFile =
+      "__test__/util/static/facebook/token_wo_email.txt";
 
-    // Read token from a file
-    let facebookAccessToken = await fsPromises.readFile(fileName, "utf-8");
+    // Extend Token Live
+    const extendTokenLive = async (fileName: string) => {
+      // Read token data from txt file name
+      let facebookAccessToken = await fsPromises.readFile(fileName, "utf-8");
 
-    // Extend Facebook Access Token Long-Live
-    const longLiveAcessToken = await axios.get(
-      `${fbGraphQLURL}/oauth/access_token?grant_type=fb_exchange_token&client_id=${appID}&client_secret=${appSecret}&fb_exchange_token=${facebookAccessToken}`
-    );
+      // Extend Facebook Access Token Long-Live
+      const extendedTokenLive = await axios.get(
+        `${fbGraphQLURL}/oauth/access_token?grant_type=fb_exchange_token&client_id=${appID}&client_secret=${appSecret}&fb_exchange_token=${facebookAccessToken}`
+      );
 
-    const newLongLivedAccessToken = longLiveAcessToken.data.access_token;
+      const longLivedTokenData = extendedTokenLive.data.access_token;
 
-    // Update new token to a file
-    await fsPromises.writeFile(fileName, newLongLivedAccessToken);
+      // Update new token to a file
+      await fsPromises.writeFile(fileName, longLivedTokenData);
 
-    return newLongLivedAccessToken;
+      return longLivedTokenData;
+    };
+
+    // Extended token data
+    const fbTokenWithEmail = await extendTokenLive(fbTokenWithEmailFile);
+    const fbTokenWithoutEmail = await extendTokenLive(fbTokenWithoutEmailFile);
+
+    return { fbTokenWithEmail, fbTokenWithoutEmail };
   } catch (err) {
     console.error(err);
   }
