7cb86ea8dd7c75a2d2c53ffe75a4e53fff812736
Keonakhon <keonakhon.kawan@outlook.com>
2021-02-01 17:21:13 +0700

    Add test case update username, get user profile

--- __test__/user.test.ts
+++ __test__/user.test.ts
@@ -79,6 +79,37 @@ const Update_My_Profile = {
   }
 };
 
+const Update_Username = {
+  mutation: gql`
+    mutation updateUsername($input: InputeUsernameUpdate) {
+      updateUsername(input: $input) {
+        user {
+          _id
+          user_id
+          username
+        }
+        errors {
+          __typename
+          ... on Unauthentication {
+            message
+          }
+          ... on ServerError {
+            message
+          }
+          ... on DatabaseError {
+            message
+          }
+        }
+      }
+    }
+  `,
+  variables: {
+    input: {
+      username: "test_username"
+    }
+  }
+};
+
 const User_Profile = {
   query: gql`
     query userProfile($username: String!) {
@@ -100,7 +131,7 @@ const User_Profile = {
       }
     }
   `,
-  variables: { username: "test" }
+  variables: { username: "test_username" }
 };
 
 describe("User", () => {
@@ -181,7 +212,61 @@ describe("User", () => {
     return done();
   });
 
-  test("Query User Profile by their username", async done => {
+  test("Query User Profile by username that not exists", async done => {
+    // Pass req/header to context
+    const req = { headers: {} };
+    const context = await createContext({ req });
+
+    // Pass context to app
+    const server = createApp(context);
+    const mockQuery = createTestClient(server).query;
+
+    const response = await mockQuery({
+      query: User_Profile.query,
+      variables: User_Profile.variables
+    });
+
+    // to remove [Object: null prototype] from each object
+    const responseString = JSON.parse(JSON.stringify(response));
+    expect(responseString.data.userProfile.user).toBeNull();
+    return done();
+  });
+
+  test("Mutation Update Username with fake Access Token", async done => {
+    // Pass req/header to context
+    const req = { headers: { authorization: `Bearer 12345678` } };
+    const context = await createContext({ req });
+
+    // Pass context to app
+    const server = createApp(context);
+    const mockMutation = createTestClient(server).mutate;
+
+    const response = await mockMutation({
+      mutation: Update_Username.mutation,
+      variables: Update_Username.variables
+    });
+
+    // to remove [Object: null prototype] from each object
+    const responseString = JSON.parse(JSON.stringify(response));
+    expect(responseString.data.updateUsername.errors).toMatchObject(
+      ErrorHandler.InvalidToken.errors
+    );
+    return done();
+  });
+
+  test("Mutation Update Username", async done => {
+    const response = await mutate({
+      mutation: Update_Username.mutation,
+      variables: Update_Username.variables
+    });
+
+    // to remove [Object: null prototype] from each object
+    const responseString = JSON.parse(JSON.stringify(response));
+    expect(responseString.data.updateUsername.user).toBeInstanceOf(Object);
+    return done();
+  });
+
+  test("Query User Profile by Username", async done => {
     // Pass req/header to context
     const req = { headers: {} };
     const context = await createContext({ req });
