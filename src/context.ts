1c484bc3a596b33c2a23f96b9c13b4cc544ed172
Keonakhon <keonakhon.kawan@outlook.com>
2021-02-20 15:53:02 +0700

    Add get ip address of request in context

--- src/context.ts
+++ src/context.ts
@@ -1,6 +1,7 @@
 // graphql context
 const createContext = async ({ req }: any) => {
   const token = req?.headers?.authorization || "";
+  const ip_address = req?.req?.ip || null;
 
   return { token };
 };
