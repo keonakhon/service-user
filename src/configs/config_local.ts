404ccd4cfcc49016debb48032ebbdd787721c089
Keonakhon <keonakhon.kawan@outlook.com>
2021-03-08 14:36:02 +0700

    Update reduce token life

--- src/configs/config_local.ts
+++ src/configs/config_local.ts
@@ -5,8 +5,8 @@ const ConfigKeyLocal = {
   access_token_secret: "423kjifos3rwsfd",
   refresh_token_secret: "fdsfjwoejri233sfwej",
   /* Format - Day * Hour * Minute * Second */
-  access_token_life: 50 * 24 * 60 * 60,
-  refresh_token_life: 60 * 24 * 60 * 60
+  access_token_life: 7 * 24 * 60 * 60,
+  refresh_token_life: 12 * 24 * 60 * 60
 };
 
 export default ConfigKeyLocal;
